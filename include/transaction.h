ddc0e865ff06365cb4f7b6f8e74e1fca5b9aaff5
Leul Neway <benjaminleul@gmail.com>
2025-05-29 17:38:09 +0300

    added all transaction features

--- include/transaction.h
+++ include/transaction.h
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-void credit(double balance, double amount){
+void credit(double &balance, double &amount){
     if(amount>balance){
         cout<< "Insufficient balance for credit." << endl;
     }
@@ -11,4 +11,29 @@ void credit(double balance, double amount){
         balance -= amount;
         cout << "Credit successful. New balance: " << balance << endl;
     }
+};
+void debit(double &balance, double &amount){
+    if(amount <= 0){
+        cout << "Debit amount must be positive." << endl;
+    }
+    else{
+        balance += amount;
+        cout << "Debit successful. New balance: " << balance << endl;
+    }
+};
+void transfer(double &from_balance, double &to_balance, double amount){
+    if(amount <= 0){
+        cout << "Transfer amount must be positive." << endl;
+    }
+    else if(amount > from_balance){
+        cout << "Insufficient balance for transfer." << endl;
+    }
+    else{
+        from_balance -= amount;
+        to_balance += amount;
+        cout << "Transfer successful. New balances: From: " << from_balance << ", To: " << to_balance << endl;
+    }
+};
+void print_balance(double balance){
+    cout << "Current balance: " << balance << endl;
 };
